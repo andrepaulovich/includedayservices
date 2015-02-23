338674eb43ac8160f070be3c20f042798ef98c64
Paulovich <paulovich@100loop.com>
2015-02-22 22:06:49 -0300

    servicos api

---
+++ IncludeDay.Services/Controllers/FeedbacksController.cs
@@ -0,0 +1,115 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using IncludeDay.Data;
+using IncludeDay.Data.Entities;
+
+namespace IncludeDay.Services.Controllers
+{
+    public class FeedbacksController : ApiController
+    {
+        private IncludeDayContext db = new IncludeDayContext();
+
+        // GET: api/Feedbacks
+        public IQueryable<Feedback> GetFeedbacks()
+        {
+            return db.Feedbacks;
+        }
+
+        // GET: api/Feedbacks/5
+        [ResponseType(typeof(Feedback))]
+        public IHttpActionResult GetFeedback(int id)
+        {
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(feedback);
+        }
+
+        // PUT: api/Feedbacks/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFeedback(int id, Feedback feedback)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != feedback.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(feedback).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FeedbackExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Feedbacks
+        [ResponseType(typeof(Feedback))]
+        public IHttpActionResult PostFeedback(Feedback feedback)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Feedbacks.Add(feedback);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = feedback.Id }, feedback);
+        }
+
+        // DELETE: api/Feedbacks/5
+        [ResponseType(typeof(Feedback))]
+        public IHttpActionResult DeleteFeedback(int id)
+        {
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            db.Feedbacks.Remove(feedback);
+            db.SaveChanges();
+
+            return Ok(feedback);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FeedbackExists(int id)
+        {
+            return db.Feedbacks.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ IncludeDay.Services/Controllers/LocationsController.cs
@@ -0,0 +1,115 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using IncludeDay.Data;
+using IncludeDay.Data.Entities;
+
+namespace IncludeDay.Services.Controllers
+{
+    public class LocationsController : ApiController
+    {
+        private IncludeDayContext db = new IncludeDayContext();
+
+        // GET: api/Locations
+        public IQueryable<Location> GetLocations()
+        {
+            return db.Locations;
+        }
+
+        // GET: api/Locations/5
+        [ResponseType(typeof(Location))]
+        public IHttpActionResult GetLocation(int id)
+        {
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(location);
+        }
+
+        // PUT: api/Locations/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLocation(int id, Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != location.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(location).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Locations
+        [ResponseType(typeof(Location))]
+        public IHttpActionResult PostLocation(Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Locations.Add(location);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = location.Id }, location);
+        }
+
+        // DELETE: api/Locations/5
+        [ResponseType(typeof(Location))]
+        public IHttpActionResult DeleteLocation(int id)
+        {
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            db.Locations.Remove(location);
+            db.SaveChanges();
+
+            return Ok(location);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LocationExists(int id)
+        {
+            return db.Locations.Count(e => e.Id == id) > 0;
+        }
+    }
+}
